0fe5292b30105b5cc0f9803756f7d4dd0230ea4a
yat8823jp <yat@yat-net.com>
2013-10-22 16:10:20 +0900

    two-first commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+function change_posts_per_page( $query ) {
+	//メインクエリでなければ
+	if ( is_admin() || ! $query->is_main_query() ){
+		return;
+	}
+ 
+	if ( $query->is_archive() ) {
+		$query->set( 'posts_per_page', '12' );
+	}
+}
+add_action( 'pre_get_posts', 'change_posts_per_page' );
